0faacd7ff919126601c3c710e733578283dd574f
200000001 <zxcvasdf1126@naver.com>
2023-01-06 19:05:58 +0900

    Create 1056.py

---
+++ 1056.py
@@ -0,0 +1,20 @@
+class Solution:
+    def confusingNumber(self, n: int) -> bool:
+        a = {
+            '0': '0',
+            '1': '1',
+            '6': '9',
+            '8': '8',
+            '9': '6',
+        }
+        
+        b = []
+        
+        str_n = str(n)
+        
+        for c in reversed(S):
+            if c not in a:
+                return False
+            b.append(a[c])
+        
+        return False if ''.join(b) == str_n else True
